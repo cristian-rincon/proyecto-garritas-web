66d69dfc0e3bbe12ba2afdc8e5996d310f5c0811
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-07-05 17:11:38 -0500

    improving frontend views

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="cGtjOrunLuewqM9mKF19SBZPeLp0VYRDskwbk78ngS2NvxROEd4HWXwLj0OtzFcP",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
